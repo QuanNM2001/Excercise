93a774ac57016d500605c995d686101319a278bf
QuanNM2001 <minhquanqn2701@gmail.com>
2022-09-28 15:40:19 +0700

    Create btMinium.js

---
+++ btMinium.js
@@ -0,0 +1,11 @@
+// Your code here.
+const max = (a, b) => {
+	return Math.max(a, b);
+}
+const min = (a, b) => {
+	return Math.min(a, b);
+}
+console.log(max(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
